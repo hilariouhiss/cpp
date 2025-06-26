35341fd806f04f9f0d9c99eb259d067811f42dd9
Liu Haiyang <hilariouhiss@outlook.com>
2025-06-26 15:07:53 +0800

    容器、const 指针

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
+#include <iostream>
 int main(int, char **) {
-  int a = 10;
-  const int *p1 = &a;
-  int *const p2 = &a;
-  //   *p1 = 100;
-  *p2 = 100;
+
+    int a{10};
+    // 指向常量的指针
+    const int *p1 = &a; // 等价 int const* ptr;
+    // *p1 = 100; 报错
+    // 常量指针
+    int *const p2 = &a;
+    *p2 = 100;
+    int b{1};
+    // p2 = &b; 报错
+
+    // constexpr if 语句
+    constexpr double gravity{9.8};
+
+    if constexpr (gravity == 9.8) {
+        std::cout << "gravity is normal\n";
+    }
 }
