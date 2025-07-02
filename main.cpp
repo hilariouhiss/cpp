6ba284b8526bdf8159c29e083f3f4fc674f05d13
刘海阳 <hilariouhiss@outlook.com>
2025-07-02 14:14:27 +0800

    格式化

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-int main(int, char **) {
-
-    int a{10};
+int main(int, char**)
+{
+    int        a{10};
     // 指向常量的指针
-    const int *p1 = &a; // 等价 int const* ptr;
+    const int* p1 = &a;  // 等价 int const* ptr;
     // *p1 = 100; 报错
     // 常量指针
-    int *const p2 = &a;
+    int* const p2 = &a;
     *p2 = 100;
-    int b{1};
+    int              b{1};
     // p2 = &b; 报错
 
     // constexpr if 语句
